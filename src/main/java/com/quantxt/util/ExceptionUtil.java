394630860e33c8797f416b52821d778151983b08
Dejan Djordjevic <djolebgd@hotmail.com>
2024-02-28 12:25:20 +0100

    PRIN-16385 When formula fail to evaluate (external document missing), try to retrieve cached value.

---
+++ src/main/java/com/quantxt/util/ExceptionUtil.java
@@ -0,0 +1,14 @@
+package com.quantxt.util;
+
+import java.util.Objects;
+
+public class ExceptionUtil {
+    public static Throwable getRootCause(Throwable throwable) {
+        Objects.requireNonNull(throwable);
+        Throwable rootCause = throwable;
+        while (rootCause.getCause() != null && rootCause.getCause() != rootCause) {
+            rootCause = rootCause.getCause();
+        }
+        return rootCause;
+    }
+}
